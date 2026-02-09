f9e623dfe43d9064730f32dbd40181ecd4b2ec02
Khang Vu <manhkhang0305@gmail.com>
2026-02-09 12:00:00 -0500

    feat: added WebElementExtensions extension class

---
+++ ReqnrollAutomation.Core/Extensions/WebElementExtensions.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * Program:         WebElementExtensions.cs
+ * Author:          Manh Khang Vu
+ * Date:            2026-07-21
+ * Description:     A class that contains extension methods for the IWebElement interface.
+ */
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace ReqnrollAutomation.Core.Extensions
+{
+    /// <summary>
+    /// A class that contains extension methods for the IWebElement interface.
+    /// </summary>
+    public static class WebElementExtensions
+    {
+        /// <summary>
+        /// Hovers over the specified IWebElement using Selenium Actions.
+        /// </summary>
+        /// <param name="element">The IWebElement to hover over.</param>
+        public static void Hover(this IWebElement element)
+        {
+            // Extract driver reference directly from the element
+            IWebDriver driver = ((IWrapsDriver)element).WrappedDriver;
+
+            Actions actions = new(driver);
+            actions.MoveToElement(element).Perform();
+        }
+    }
+}
