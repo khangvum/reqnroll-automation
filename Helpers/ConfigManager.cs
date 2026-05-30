81c5282908cbe5d4b8bd590acedd06692c2e520a
Khang Vu <manhkhang0305@gmail.com>
2026-05-30 09:46:05 -0400

    feat: added ConfigManager

---
+++ Helpers/ConfigManager.cs
@@ -0,0 +1,19 @@
+﻿/**
+ * Program:         ConfigManager.cs
+ * Author:          Manh Khang Vu
+ * Date:            2026-05-26
+ * Description:     A class that manages the configuration settings for the Reqnroll automation framework.
+ */
+
+namespace ReqnrollAutomation.Helpers
+{
+    /// <summary>
+    /// A class that manages the configuration settings for the Reqnroll automation framework.
+    /// </summary>
+    internal static class ConfigManager
+    {
+        #region Public Properties
+        public static int DefaultTimeout => 30;
+        #endregion
+    }
+}
