c5013e4dbe9264d49b992c43832a19a22fda910c
Khang Vu <manhkhang0305@gmail.com>
2026-06-02 21:32:43 -0400

    feat: added GlobalUsings.cs

---
+++ ReqnrollAutomation/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using OpenQA.Selenium;
+global using Reqnroll;
+
+// Parallel execution
+// ExecutionScope.MethodLevel    - Every scenario runs in parallel (fastest, requires fully isolated scenarios)
+// ExecutionScope.ClassLevel     - Features run in parallel, but scenarios within a feature run sequentially (slower, but allows sharing state between scenarios)
+[assembly: Parallelize(Workers = 4, Scope = ExecutionScope.ClassLevel)]
