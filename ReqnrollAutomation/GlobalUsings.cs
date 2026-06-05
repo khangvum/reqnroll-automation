21bf72b85d5e27aeef70fcb9ca04802d67810e37
Khang Vu <manhkhang0305@gmail.com>
2026-06-04 21:28:00 -0400

    chore: set number of concurrent workers to 2

--- ReqnrollAutomation/GlobalUsings.cs
+++ ReqnrollAutomation/GlobalUsings.cs
@@ -5,4 +5,4 @@ global using Reqnroll;
 // Parallel execution
 // ExecutionScope.MethodLevel    - Every scenario runs in parallel (fastest, requires fully isolated scenarios)
 // ExecutionScope.ClassLevel     - Features run in parallel, but scenarios within a feature run sequentially (slower, but allows sharing state between scenarios)
-[assembly: Parallelize(Workers = 4, Scope = ExecutionScope.ClassLevel)]
+[assembly: Parallelize(Workers = 2, Scope = ExecutionScope.ClassLevel)]
