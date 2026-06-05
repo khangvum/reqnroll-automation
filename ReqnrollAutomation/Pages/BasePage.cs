2e49b4fe654fd74702f9dd659654f672be50dc86
Khang Vu <manhkhang0305@gmail.com>
2026-06-04 21:11:03 -0400

    feat: added BasePage class

---
+++ ReqnrollAutomation/Pages/BasePage.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * Program:         BasePage.cs
+ * Author:          Manh Khang Vu
+ * Date:            2026-06-04
+ * Description:     A class that represents the base page object model for the web application under test, 
+ *                  providing common functionality and properties for all page objects.
+ */
+
+using OpenQA.Selenium.Support.UI;
+using ReqnrollAutomation.Helpers;
+
+namespace ReqnrollAutomation.Pages
+{
+    /// <summary>
+    /// A class that represents the base page object model for the web application under test, 
+    /// providing common functionality and properties for all page objects.
+    /// </summary>
+    internal class BasePage
+    {
+        #region Protected Attributes
+        protected readonly IWebDriver _driver;
+        protected readonly WebDriverWait _wait;
+        #endregion
+
+        #region Constructor
+        public BasePage(IWebDriver driver)
+        {
+            _driver = driver ?? throw new ArgumentNullException(nameof(driver));
+            _wait = new(_driver, TimeSpan.FromSeconds(ConfigManager.DefaultTimeout));
+        }
+        #endregion
+    }
+}
