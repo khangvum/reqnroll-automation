aa989b4973a42c698ecd2e0ddd782b832025f718
Khang Vu <manhkhang0305@gmail.com>
2026-06-12 22:40:53 -0400

    chore: added Navigate and LoginWithCredentials methods

--- ReqnrollAutomation/Pages/LoginPage.cs
+++ ReqnrollAutomation/Pages/LoginPage.cs
@@ -12,6 +12,10 @@ namespace ReqnrollAutomation.Pages
 {
     internal class LoginPage : BasePage
     {
+        #region Private Attributes
+        private const string PageUrl = "https://www.saucedemo.com/";
+        #endregion
+
         #region Page Locators
         private readonly By _usernameFieldLocator = By.Id("user-name");
         private readonly By _passwordFieldLocator = By.Id("password");
@@ -33,11 +37,33 @@ namespace ReqnrollAutomation.Pages
         #endregion
 
         #region Public Methods
+        /// <summary>
+        /// Navigates to the login page.
+        /// </summary>
+        public void Navigate()
+        {
+            _driver.Navigate().GoToUrl(PageUrl);
+        }
+
+        /// <summary>
+        /// Logs in using the credentials associated with the specified role.
+        /// </summary>
+        /// <param name="role">The role for which to use credentials.</param>
         public void LoginAsRole(string role)
         {
             string username = CredentialManager.Credentials.Accounts[role];
             string password = CredentialManager.Credentials.SharedPassword;
 
+            LoginWithCredentials(username, password);
+        }
+
+        /// <summary>
+        /// Logs in using the specified username and password.
+        /// </summary>
+        /// <param name="username">The username to use for login.</param>
+        /// <param name="password">The password to use for login.</param>
+        public void LoginWithCredentials(string username, string password)
+        {
             UsernameField.SendKeys(username);
             PasswordField.SendKeys(password);
             LoginButton.Click();
