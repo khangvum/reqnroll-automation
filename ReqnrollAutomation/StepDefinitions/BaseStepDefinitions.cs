e2ed64b39005a4467ac545e7c3076f8fe3c34efe
Khang Vu <manhkhang0305@gmail.com>
2026-06-12 21:12:38 -0400

    feat: added BaseStepDefinitions class

---
+++ ReqnrollAutomation/StepDefinitions/BaseStepDefinitions.cs
@@ -0,0 +1,42 @@
+﻿
+
+using ReqnrollAutomation.Core.Extensions;
+
+/**
+ * Program:         BaseStepDefinitions.cs
+ * Author:          Manh Khang Vu
+ * Date:            2026-06-11
+ * Description:     A class that defines base step definitions for the automation framework.
+ */
+namespace ReqnrollAutomation.StepDefinitions
+{
+    public class BaseStepDefinitions
+    {
+        #region Private Attributes
+        // Driver & Contexts
+        private IWebDriver? _driver;
+        private readonly ScenarioContext _scenarioContext;
+        private readonly FeatureContext _featureContext;
+        #endregion
+
+        #region Public Properties
+        // Lazy initialization of the WebDriver instance from the ScenarioContext to ensure it is only retrieved when needed
+        public IWebDriver Driver
+        {
+            get
+            {
+                _driver ??= _scenarioContext.GetDriver();
+                return _driver;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public BaseStepDefinitions(ScenarioContext scenarioContext, FeatureContext featureContext)
+        {
+            _scenarioContext = scenarioContext ?? throw new ArgumentNullException(nameof(scenarioContext));
+            _featureContext = featureContext ?? throw new ArgumentNullException(nameof(featureContext));
+        }
+        #endregion
+    }
+}
