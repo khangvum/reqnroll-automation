da5e652705dac6cfc890e51cb89827600998e7f4
Khang Vu <manhkhang0305@gmail.com>
2026-07-10 21:40:30 -0400

    feat: added IConfigProvider

---
+++ ReqnrollAutomation.Core/Config/IConfigAdapter.cs
@@ -0,0 +1,27 @@
+﻿/**
+ * Program:         IConfigAdapter.cs
+ * Author:          Manh Khang Vu
+ * Date:            2026-07-11
+ * Description:     An interface for adapting configuration settings for the 
+ *                  Reqnroll automation framework from external sources.
+ */
+
+namespace ReqnrollAutomation.Core.Config
+{
+    /// <summary>
+    /// An interface for adapting configuration settings for the
+    /// Reqnroll automation framework from external sources.
+    /// </summary>
+    public interface IConfigAdapter
+    {
+        /// <summary>
+        /// Gets the configured environment.
+        /// </summary>
+        string Environment { get; }
+
+        /// <summary>
+        /// Gets the configured browser type.
+        /// </summary>
+        BrowserType Browser { get; }
+    }
+}
---
+++ ReqnrollAutomation/Config/ConfigAdapter.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * Program:         ConfigAdapter.cs
+ * Author:          Manh Khang Vu
+ * Date:            2026-07-11
+ * Description:     An interface for adapting configuration settings for the 
+ *                  Reqnroll automation framework from external sources.
+ */
+
+using ReqnrollAutomation.Core.Config;
+
+namespace ReqnrollAutomation.Config
+{
+    /// <summary>
+    /// An interface for adapting configuration settings for the 
+    /// Reqnroll automation framework from external sources.
+    /// </summary>
+    internal class ConfigAdapter : IConfigAdapter
+    {
+        /// <inheritdoc/>
+        public string Environment => ConfigManager.Environment;
+
+        /// <inheritdoc/>
+        public BrowserType Browser => ConfigManager.Browser;
+    }
+}
