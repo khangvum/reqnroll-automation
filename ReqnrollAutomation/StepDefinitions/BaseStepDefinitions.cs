e80dca226b39f7b8d9c7b24ce352669e61db3aa0
Khang Vu <manhkhang0305@gmail.com>
2026-07-17 19:51:50 -0400

    chore: moved BaseStepDefinitions to the root of StepDefinitions folder

---
+++ ReqnrollAutomation/StepDefinitions/BaseStepDefinitions.cs
@@ -0,0 +1,44 @@
+﻿/**
+ * Program:         BaseStepDefinitions.cs
+ * Author:          Manh Khang Vu
+ * Date:            2026-06-11
+ * Description:     A class that defines base step definitions for the automation framework.
+ */
+
+using ReqnrollAutomation.Core.Extensions;
+using ReqnrollAutomation.Pages.SwagLabs;
+
+namespace ReqnrollAutomation.StepDefinitions
+{
+    public class BaseStepDefinitions
+    {
+        #region Private Attributes
+        // Driver & Contexts
+        private IWebDriver? _driver;
+        private readonly ScenarioContext _scenarioContext;
+        private readonly FeatureContext _featureContext;
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        /// Lazy initialization of the WebDriver instance from the ScenarioContext.
+        /// </summary>
+        public IWebDriver Driver
+        {
+            get
+            {
+                _driver ??= _scenarioContext.GetDriver();
+                return _driver;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public BaseStepDefinitions(ScenarioContext scenarioContext, FeatureContext featureContext)
+        {
+            _scenarioContext = scenarioContext ?? throw new ArgumentNullException(nameof(scenarioContext));
+            _featureContext = featureContext ?? throw new ArgumentNullException(nameof(featureContext));
+        }
+        #endregion
+    }
+}
