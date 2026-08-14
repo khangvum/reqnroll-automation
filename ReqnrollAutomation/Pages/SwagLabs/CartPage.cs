e14595a0437c140864a0bc0cf20e715d32175971
Khang Vu <manhkhang0305@gmail.com>
2026-08-14 01:07:05 -0400

    feat: added CartPage

---
+++ ReqnrollAutomation/Pages/SwagLabs/CartPage.cs
@@ -0,0 +1,65 @@
+﻿/**
+ * Program:         CartPage.cs
+ * Author:          Manh Khang Vu
+ * Date:            2026-08-18
+ * Description:     A class that represents the cart page of Swag Labs.
+ */
+
+namespace ReqnrollAutomation.Pages.SwagLabs
+{
+    /// <summary>
+    /// A class that represents the cart page of Swag Labs.
+    /// </summary>
+    public class CartPage : BasePage
+    {
+        #region Public Properties
+        // URL
+        public override string PageUrl => "https://www.saucedemo.com/cart.html";
+        #endregion
+
+        #region Page Locators
+        // Cart List locators
+        private readonly By _cartItemLocator = By.CssSelector("div.cart_item");
+
+        // Cart Item locators (relative to each cart item)
+        private readonly By _itemNameLocator = By.CssSelector(".inventory_item_name");
+        private readonly By _itemDescLocator = By.CssSelector(".inventory_item_desc");
+        private readonly By _itemPriceLocator = By.CssSelector(".inventory_item_price");
+        #endregion
+
+        #region Page Elements
+        // Cart List elements
+        public IReadOnlyList<IWebElement> CartItems => _driver.FindElements(_cartItemLocator);
+        #endregion
+
+        #region Constructor
+        public CartPage(IWebDriver driver) : base(driver)
+        {
+        }
+        #endregion
+
+        #region Page Methods
+        /// <summary>
+        /// Gets the details of all items in the cart, including their name, description, and price.
+        /// </summary>
+        /// <returns>A list of tuples containing the name, description, and price of each item in the cart.</returns>
+        public IReadOnlyList<(string Name, string Description, decimal Price)> GetCartItemsDetails()
+        {
+            List<(string Name, string Description, decimal Price)> cartItemsDetails = [];
+            foreach (IWebElement cartItem in CartItems)
+            {
+                // Get the details of each item
+                string name = cartItem.FindElement(_itemNameLocator).Text;
+                string description = cartItem.FindElement(_itemDescLocator).Text;
+                string priceText = cartItem.FindElement(_itemPriceLocator).Text.Replace("$", "");
+                decimal price = decimal.Parse(priceText);
+
+                // Store the details of the item
+                cartItemsDetails.Add((name, description, price));
+            }
+
+            return cartItemsDetails;
+        }
+        #endregion
+    }
+}
