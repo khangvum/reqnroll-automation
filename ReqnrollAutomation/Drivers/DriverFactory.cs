57e426e9128914694de416206a5c5bbde0b7ba29
Khang Vu <manhkhang0305@gmail.com>
2026-08-15 10:55:24 -0400

    chore: cleaned up initialization

--- ReqnrollAutomation/Drivers/DriverFactory.cs
+++ ReqnrollAutomation/Drivers/DriverFactory.cs
@@ -22,7 +22,7 @@ namespace ReqnrollAutomation.Drivers
     {
         #region Private Attributes
         // ThreadLocal ensures each worker thread manages its own isolated IWebDriver instance
-        private static readonly ThreadLocal<IWebDriver?> LocalDriver = new ThreadLocal<IWebDriver?>(trackAllValues: true);
+        private static readonly ThreadLocal<IWebDriver?> LocalDriver = new(trackAllValues: true);
         #endregion
 
         #region Public Methods
