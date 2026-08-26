85a49e51d668c7637ff99e5f16fa245a539d8feb
Khang Vu <manhkhang0305@gmail.com>
2026-08-26 12:00:00 -0400

    feat: added GetCheckoutOverviewItemsDetails method

--- ReqnrollAutomation/Pages/SwagLabs/CheckoutPage.cs
+++ ReqnrollAutomation/Pages/SwagLabs/CheckoutPage.cs
@@ -1,4 +1,8 @@
-﻿/**
+﻿
+
+using ReqnrollAutomation.Models.SwagLabs;
+
+/**
  * Program:         CartPage.cs
  * Author:          Manh Khang Vu
  * Date:            2026-08-20
@@ -35,31 +39,44 @@ namespace ReqnrollAutomation.Pages.SwagLabs
         #endregion
 
         #region Page Locators
-        // Checkout Step One Page locators
+        // 1. Checkout Step One Page locators
+        // - Input Field locators
         private readonly By _firstNameInputLocator = By.CssSelector("input#first-name");
         private readonly By _lastNameInputLocator = By.CssSelector("input#last-name");
         private readonly By _postalCodeInputLocator = By.CssSelector("input#postal-code");
         private readonly By _continueButtonLocator = By.CssSelector("input#continue");
 
-        // Checkout Step Two Page locators
+        // - Cart List locators
+        private readonly By _cartItemLocator = By.CssSelector("div.cart_item");
+
+        // - Cart Item locators (relative to each cart item)
+        private readonly By _itemNameLocator = By.CssSelector(".inventory_item_name");
+        private readonly By _itemDescLocator = By.CssSelector(".inventory_item_desc");
+        private readonly By _itemPriceLocator = By.CssSelector(".inventory_item_price");
+
+        // 2. Checkout Step Two Page locators
         private readonly By _finishButtonLocator = By.CssSelector("button#finish");
 
-        // Checkout Completion Page locators
+        // 3. Checkout Completion Page locators
         private readonly By _orderConfirmationHeaderLocator = By.CssSelector("h2.complete-header");
         private readonly By _orderConfirmationMessageLocator = By.CssSelector("div.complete-text");
         #endregion
 
         #region Page Elements
-        // Checkout Step One Page elements
+        // 1. Checkout Step One Page elements
+        // - Input Field elements
         private IWebElement FirstNameInput => _driver.FindElement(_firstNameInputLocator);
         private IWebElement LastNameInput => _driver.FindElement(_lastNameInputLocator);
         private IWebElement PostalCodeInput => _driver.FindElement(_postalCodeInputLocator);
         private IWebElement ContinueButton => _driver.FindElement(_continueButtonLocator);
 
-        // Checkout Step Two Page elements
+        // - Cart List elements
+        private IReadOnlyList<IWebElement> CartItems => _driver.FindElements(_cartItemLocator);
+
+        // 2. Checkout Step Two Page elements
         private IWebElement FinishButton => _driver.FindElement(_finishButtonLocator);
 
-        // Checkout Completion Page elements
+        // 3. Checkout Completion Page elements
         private IWebElement OrderConfirmationHeader => _driver.FindElement(_orderConfirmationHeaderLocator);
         private IWebElement OrderConfirmationMessage => _driver.FindElement(_orderConfirmationMessageLocator);
         #endregion
@@ -114,6 +131,33 @@ namespace ReqnrollAutomation.Pages.SwagLabs
         /// </summary>
         /// <returns>The text of the order confirmation message.</returns>
         public string GetOrderConfirmationMessage() => OrderConfirmationMessage.Text;
+
+        /// <summary>
+        /// Gets the details of all items in the checkout overview, including their name, description, and price.
+        /// </summary>
+        /// <returns>A list of InventoryItemDetails objects containing the name, description, and price of each item in the checkout overview.</returns>
+        public IReadOnlyList<InventoryItemDetails> GetCheckoutOverviewItemsDetails()
+        {
+            List<InventoryItemDetails> cartItemsDetails = [];
+            foreach (IWebElement cartItem in CartItems)
+            {
+                // Get the details of each item
+                string name = cartItem.FindElement(_itemNameLocator).Text;
+                string description = cartItem.FindElement(_itemDescLocator).Text;
+                string priceText = cartItem.FindElement(_itemPriceLocator).Text.Replace("$", "");
+                decimal price = decimal.Parse(priceText);
+
+                // Store the details of the item
+                cartItemsDetails.Add(new InventoryItemDetails
+                {
+                    Name = name,
+                    Description = description,
+                    Price = price,
+                });
+            }
+
+            return cartItemsDetails;
+        }
         #endregion
 
         #region Private Helper Methods
