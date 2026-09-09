58e0c645a5530529a0282c7fc3d8dfd2ce06367d
Khang Vu <manhkhang0305@gmail.com>
2026-09-09 12:00:00 -0400

    chore: updated file name in documentation

--- ReqnrollAutomation/StepDefinitions/KhangvumPortfolio/HeaderComponentStepDefinitions.cs
+++ ReqnrollAutomation/StepDefinitions/KhangvumPortfolio/HeaderComponentStepDefinitions.cs
@@ -1,5 +1,5 @@
 ﻿/**
- * Program:         HeaderStepDefinitions.cs
+ * Program:         HeaderComponentStepDefinitions.cs
  * Author:          Manh Khang Vu
  * Date:            2026-09-09
  * Description:     A class that defines the step definitions for the header component verification feature on the Khangvum Portfolio website.
