26a317dd1c4c22edbb0c09846eb49ec6294f8282
Khang Vu <manhkhang0305@gmail.com>
2026-09-09 12:00:00 -0400

    feat: added footer locators, elements, and methods

--- ReqnrollAutomation/Pages/KhangvumPortfolio/PortfolioPage.cs
+++ ReqnrollAutomation/Pages/KhangvumPortfolio/PortfolioPage.cs
@@ -20,6 +20,17 @@ namespace ReqnrollAutomation.Pages.KhangvumPortfolio
             new(){ Section = "Projects", ExpectedUrl = "https://khangvum.com/#projects" },
             new(){ Section = "Contact", ExpectedUrl = "https://khangvum.com/#contact" }
         ];
+
+        // - Footer social media links
+        /// <summary>
+        /// A read-only list of social media links in the footer of the Khangvum Portfolio page.
+        /// </summary>
+        public readonly IReadOnlyList<SocialMediaLink> SocialMediaLinks =
+        [
+            new() { Platform = "GitHub", ExpectedUrl = "https://github.com/khangvum" },
+            //new() { Platform = "LinkedIn", ExpectedUrl = "https://linkedin.com/in/khangvum" },    // LinkedIn will force to login page to view personal profiles
+            new() { Platform = "Gmail", ExpectedUrl = "mailto:manhkhang0305@gmail.com" }
+        ];
         #endregion
 
         #region Page Locators
@@ -28,32 +39,48 @@ namespace ReqnrollAutomation.Pages.KhangvumPortfolio
         /// Gets the locator for a header section link based on the provided section name.
         /// </summary>
         /// <param name="section">The name of the section for which to get the locator.</param>
-        /// <returns>The By object representing the locator for the header section link.</returns>
+        /// <returns>The locator for the header section link.</returns>
         private By GetHeaderSectionLocator(string section) => By.XPath($"//a[contains(@class, 'nav-link') and text()='{section}']");
 
         /// <summary>
         /// Gets the locator for a section on the page based on the provided section name.
         /// </summary>
         /// <param name="section">The name of the section for which to get the locator.</param>
-        /// <returns>The By object representing the locator for the section.</returns>
+        /// <returns>The locator for the section.</returns>
         private By GetSectionLocator(string section) => By.Id(section.ToLower());
+
+        // Footer locators
+        /// <summary>
+        /// Gets the locator for a social media button in the footer based on the provided platform name.
+        /// </summary>
+        /// <param name="platform">The name of the social media platform.</param>
+        /// <returns>The locator for the social media button.</returns>
+        private By GetFooterSocialMediaButtonLocator(string platform) => By.XPath($"//a[contains(@class, 'contact-icon-btn') and contains(@href, '{platform.ToLower()}')]");
         #endregion
 
         #region Page Elements
         // Header elements
         /// <summary>
-        /// Gets the IWebElement for a header section link based on the provided section name.
+        /// Gets the element for a header section link based on the provided section name.
         /// </summary>
         /// <param name="section">The name of the section for which to get the element.</param>
-        /// <returns>The IWebElement representing the header section link.</returns>
+        /// <returns>The header section link element.</returns>
         private IWebElement GetHeaderSectionElement(string section) => _driver.WaitAndFindElement(GetHeaderSectionLocator(section));
 
         /// <summary>
-        /// Gets the IWebElement for a section on the page based on the provided section name.
+        /// Gets the element for a section on the page based on the provided section name.
         /// </summary>
         /// <param name="section">The name of the section for which to get the element.</param>
-        /// <returns>The IWebElement representing the section.</returns>
+        /// <returns>The section element.</returns>
         private IWebElement GetSectionElement(string section) => _driver.WaitAndFindElement(GetSectionLocator(section));
+
+        // Footer elements
+        /// <summary>
+        /// Gets the element for a social media button in the footer based on the provided platform name.
+        /// </summary>
+        /// <param name="platform">The name of the social media platform.</param>
+        /// <returns>The social media button element.</returns>
+        private IWebElement GetFooterSocialMediaButton(string platform) => _driver.WaitAndFindElement(GetFooterSocialMediaButtonLocator(platform));
         #endregion
 
         #region Constructor
@@ -74,6 +101,12 @@ namespace ReqnrollAutomation.Pages.KhangvumPortfolio
         }
 
         /// <summary>
+        /// Clicks on a social media button in the footer based on the provided platform name.
+        /// </summary>
+        /// <param name="platform">The name of the social media platform.</param>
+        public void ClickFooterSocialMediaButton(string platform) => GetFooterSocialMediaButton(platform).Click();
+
+        /// <summary>
         /// Checks if a section is in the viewport based on the provided section name.
         /// </summary>
         /// <param name="section">The name of the section to check.</param>
@@ -91,6 +124,11 @@ namespace ReqnrollAutomation.Pages.KhangvumPortfolio
                 return (rect.top <= windowHeight && rect.bottom >= 0);
                 """, sectionElement)!;
         }
+
+        /// <summary>
+        /// Scrolls the page to the footer section.
+        /// </summary>
+        public void ScrollToFooter() => ScrollIntoView(GetSectionElement(HeaderLinks[^1].Section));
         #endregion
     }
 }
