7c76a59ba5ee3e52cd60e20e7542701b0b6f9d78
Khang Vu <manhkhang0305@gmail.com>
2026-09-18 23:08:01 -0400

    feat: added project card list, locators, elements, and methods

--- ReqnrollAutomation/Pages/KhangvumPortfolio/PortfolioPage.cs
+++ ReqnrollAutomation/Pages/KhangvumPortfolio/PortfolioPage.cs
@@ -21,6 +21,20 @@ namespace ReqnrollAutomation.Pages.KhangvumPortfolio
             new(){ Section = "Contact", ExpectedUrl = "https://khangvum.com/#contact" }
         ];
 
+        // - Projects links
+        /// <summary>
+        /// A read-only list of project links on the Khangvum Portfolio page, each containing a project name and its expected URL.
+        /// </summary>
+        public readonly IReadOnlyList<ProjectCard> ProjectCards =
+        [
+            new(){ ProjectName = "Infrastructure as Code (IaC) Homelab", ExpectedUrl = "https://github.com/khangvum/homelab-iac" },
+            new(){ ProjectName = "Answer Files", ExpectedUrl = "https://github.com/khangvum/answer-files" },
+            new(){ ProjectName = "Reqnroll Automation", ExpectedUrl = "https://github.com/khangvum/reqnroll-automation" },
+            new(){ ProjectName = "Expression Evaluator", ExpectedUrl = "https://github.com/khangvum/exprevaluator" },
+            new(){ ProjectName = "NixOS-WSL Configuration", ExpectedUrl = "https://github.com/khangvum/nixos-wsl" },
+            new(){ ProjectName = "Chemical Equation Balancer", ExpectedUrl = "https://github.com/khangvum/chemicalbalancer" }
+        ];
+
         // - Footer social media links
         /// <summary>
         /// A read-only list of social media links in the footer of the Khangvum Portfolio page.
@@ -49,6 +63,14 @@ namespace ReqnrollAutomation.Pages.KhangvumPortfolio
         /// <returns>The locator for the section.</returns>
         private By GetSectionLocator(string section) => By.Id(section.ToLower());
 
+        // Projects locator
+        /// <summary>
+        /// Gets the locator for a project card based on the provided index.
+        /// </summary>
+        /// <param name="index">The index of the project card for which to get the locator.</param>
+        /// <returns>The locator for the project card.</returns>
+        private By GetStationCardLocator(Index index) => By.XPath($"//div[contains(@class, 'stations-force-grid')]/div[contains(@class, 'station-node')][{index.GetOffset(ProjectCards.Count) + 1}]");
+
         // Footer locators
         /// <summary>
         /// Gets the locator for a social media button in the footer based on the provided platform name.
@@ -73,6 +95,14 @@ namespace ReqnrollAutomation.Pages.KhangvumPortfolio
         /// <param name="section">The name of the section for which to get the element.</param>
         /// <returns>The section element.</returns>
         private IWebElement GetSectionElement(string section) => _driver.WaitAndFindElement(GetSectionLocator(section));
+
+        // Project elements
+        /// <summary>
+        /// Gets the element for a project card based on the provided index.
+        /// </summary>
+        /// <param name="index">The index of the project card for which to get the element.</param>
+        /// <returns>The project card element.</returns>
+        private IWebElement GetStationCardElement(Index index) => _driver.WaitAndFindElement(GetStationCardLocator(index));
 
         // Footer elements
         /// <summary>
@@ -107,6 +137,23 @@ namespace ReqnrollAutomation.Pages.KhangvumPortfolio
         public void ClickFooterSocialMediaButton(string platform) => GetFooterSocialMediaButton(platform).Click();
 
         /// <summary>
+        /// CLicks on a project card based on the provided index.
+        /// </summary>
+        /// <param name="index">The index of the project card to click.</param>
+        public void ClickProjectCard(Index index) => GetStationCardElement(index).Click();
+
+        /// <summary>
+        /// Clicks on a random project card from the list of project links and returns the corresponding ProjectLink object.
+        /// </summary>
+        /// <returns>The ProjectLink object for the clicked project card.</returns>
+        public ProjectCard ClickRandomProjectCard()
+        {
+            int randomIndex = Random.Shared.Next(ProjectCards.Count);
+            ClickProjectCard(randomIndex);
+            return ProjectCards[randomIndex];
+        }
+
+        /// <summary>
         /// Checks if a section is in the viewport based on the provided section name.
         /// </summary>
         /// <param name="section">The name of the section to check.</param>
@@ -129,6 +176,11 @@ namespace ReqnrollAutomation.Pages.KhangvumPortfolio
         /// Scrolls the page to the footer section.
         /// </summary>
         public void ScrollToFooter() => ScrollIntoView(GetSectionElement(HeaderLinks[^1].Section));
+
+        /// <summary>
+        /// Scrolls the page to the "Projects" section.
+        /// </summary>
+        public void ScrollToProjectsSection() => ScrollIntoView(GetSectionElement(HeaderLinks[2].Section));
         #endregion
     }
 }
